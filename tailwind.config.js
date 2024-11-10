801d136c78670d945d08bb74aae9cd59d494eebd
Koushik-su <koushik15-14690@diu.edu.bd>
2024-11-10 14:29:05 +0600

    pantwebsite

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        screens: {
+            sm: "340px",
+            md: "540px",
+            lg: "768px",
+            xl: "1180px",
+          },
+        extend: {},
+    },
+    plugins: [],
+}
